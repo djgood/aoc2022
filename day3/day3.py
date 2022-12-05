bca4e89c24df1e084d3a206e26bd9c20960be1cd
Desmond Good <desmond.good@enlightedinc.com>
2022-12-04 21:13:23 -0500

    Add day3 solution

---
+++ day3/day3.py
@@ -0,0 +1,49 @@
+import itertools
+from typing import Iterator, List
+
+def read_file(filename: str) -> Iterator[str]:
+    with open(filename) as file:
+        for line in file:
+            yield line.strip()
+
+def chunked_iterable(iterable, size):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, size))
+        if not chunk:
+            break
+        yield chunk
+
+def priority(letter: str) -> int:
+    char = ord(letter)
+    if ord('a') <= char and char <= ord('z'):
+        priority = char - 96
+
+    elif ord('A') <= char and char <= ord('Z'):
+        priority = char - 64 + 26
+    else:
+        raise Exception("pls help")
+
+    return priority
+
+class Priorities:
+    part1: List[int] = []
+    part2: List[int] = []
+
+priorities = Priorities()
+
+for rucksack in read_file("input.txt"):
+    compartment_len = int(len(rucksack)/2)
+    rucksack = (rucksack[0:compartment_len], rucksack[compartment_len:])
+    rucksack = (set(rucksack[0]), set(rucksack[1]))
+    same = rucksack[0].intersection(rucksack[1])
+    priorities.part1.append(priority(same.pop()))
+
+for rucksacks in chunked_iterable(read_file("input.txt"), 3):
+    rucksacks_sets = [set(rucksack) for rucksack in rucksacks]
+    same = rucksacks_sets[0].intersection(rucksacks_sets[1]).intersection(rucksacks_sets[2])
+    priorities.part2.append(priority(same.pop()))
+
+print(sum(priorities.part1))
+print(sum(priorities.part2))
+
